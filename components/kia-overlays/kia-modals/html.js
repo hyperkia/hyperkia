2987ca1c3400c28f3f9981767de8675d18f8b210
Pardip Lal <pardiplaljio@gmail.com>
2026-04-04 20:14:08 +0530

    update ui

--- components/kia-overlays/kia-modals/html.js
+++ components/kia-overlays/kia-modals/html.js
@@ -6,9 +6,7 @@ import "./modals/kia-missing-fonts-modal/index.js";
 import "./modals/kia-assets-manager-modal/index.js";
 
 const html = `
-	<div class="modals" data-id="modals">
-		<kia-file-import-modal class="modal show" data-class="modal"></kia-file-import-modal>
-	</div>
+	<div class="modals" data-id="modals"></div>
 `;
 
 export default html;
